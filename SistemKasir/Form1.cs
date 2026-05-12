c0f8ee34c21bde9c7fde719f7ed26ccaf210d4e3
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2026-05-13 01:57:17 +0700

    menambahkan bs datasource di txtcari

--- SistemKasir/Form1.cs
+++ SistemKasir/Form1.cs
@@ -291,7 +291,8 @@ namespace SistemKasir
                 SqlDataReader dr = cmd.ExecuteReader();
                 DataTable dt = new DataTable();
                 dt.Load(dr);
-                dgvBarang.DataSource = dt;
+                bs.DataSource = dt;
+                dgvBarang.DataSource = bs;
 
                 conn.Close();
             }
